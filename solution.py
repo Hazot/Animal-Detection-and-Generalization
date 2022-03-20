8233bfd12ca74a3a63221a5f394f983840f778ea
Hazot <kevinlessard74@gmail.com>
2022-03-20 01:10:07 -0400

    nice update

---
+++ solution.py
@@ -0,0 +1,48 @@
+import json
+import matplotlib.pyplot as plt
+import torch
+
+import torchvision
+from torch.utils.data import Dataset, DataLoader
+from torchvision.transforms import ToTensor
+import os
+from torchvision.io import read_image
+from PIL import Image
+
+import numpy as np
+import matplotlib.patches as patches
+from torchvision import transforms
+
+from torchvision.models.detection.faster_rcnn import FastRCNNPredictor
+
+from torchmetrics.detection.map import MeanAveragePrecision
+import pycocotools
+
+# imports from local lib files
+import utils
+import transforms
+import coco_eval
+from engine import train_one_epoch, evaluate
+
+# Set the paths
+output_path = 'output'
+img_folder = 'eccv_18_all_images_sm'
+
+cis_test_ann_path = 'eccv_18_annotation_files/cis_test_annotations.json'
+cis_val_ann_path = 'eccv_18_annotation_files/cis_val_annotations.json'
+train_ann_path = 'eccv_18_annotation_files/train_annotations.json'
+trans_test_ann_path = 'eccv_18_annotation_files/trans_test_annotations.json'
+trans_val_ann_path = 'eccv_18_annotation_files/trans_val_annotations.json'
+
+# Load the data
+cis_test_ann = json.load(open(cis_test_ann_path))
+cis_val_ann = json.load(open(cis_val_ann_path))
+train_ann= json.load(open(train_ann_path))
+trans_test_ann = json.load(open(trans_test_ann_path))
+trans_val_ann = json.load(open(trans_val_ann_path))
+
+print(len(cis_test_ann['images']))
+print(len(cis_val_ann['images']))
+print(len(train_ann['images']))
+print(len(trans_test_ann['images']))
+print(len(trans_val_ann['images']))
