3d60277b8385ab0b179cf19b229a8ddf83c6f448
Hazot <kevinlessard74@gmail.com>
2022-04-27 01:58:24 -0400

    more results

--- scripts/animal_detection.py
+++ scripts/animal_detection.py
@@ -58,6 +58,73 @@ print(len(trans_test_ann['images']))
 print(len(trans_val_ann['images']))
 
 
+# 
+class CustomImageDataset(Dataset):
+    def __init__(self, label_path, img_dir, valid_img, transform = None):
+        self.label_file = json.load(open(label_path))
+        self.img_dir = img_dir
+        self.transform = transform
+        self.valid_img = valid_img
+
+    def __len__(self):
+        return len(self.valid_img)
+
+    def __getitem__(self, idx):
+        
+        idx = self.valid_img[idx] # consider only images with bbox annotations
+        img_path = os.path.join(self.img_dir, self.label_file['images'][idx]['file_name'])
+        image = read_image(img_path)
+
+        conv = torchvision.transforms.ToTensor()
+        # if image.shape[0]==1:
+        # some images have only one channel, we convert them to rgb
+        image = Image.open(img_path).convert("RGB")
+        image = conv(image)
+
+        boxes = [self.label_file['annotations'][j]['bbox'] 
+                 for j in range(len(self.label_file['annotations'])) 
+                 if self.label_file['annotations'][j]['image_id']==self.label_file['images'][idx]['id']]
+        
+        label = [self.label_file['annotations'][j]['category_id'] 
+                 for j in range(len(self.label_file['annotations'])) 
+                 if self.label_file['annotations'][j]['image_id']==self.label_file['images'][idx]['id']]
+
+        # transform bbox coords to adjust for resizing
+        scale_x = image.shape[2] / self.label_file['images'][idx]['width'] 
+        scale_y = image.shape[1] / self.label_file['images'][idx]['height']
+
+        boxes = torch.as_tensor(boxes)
+        for i in range(boxes.shape[0]):
+            boxes[i][0] = torch.round(boxes[i][0] * scale_x)
+            boxes[i][1] = torch.round(boxes[i][1] * scale_y)
+            boxes[i][2] = torch.round(boxes[i][2] * scale_x)
+            boxes[i][3] = torch.round(boxes[i][3] * scale_y)
+
+            boxes[i][2] = boxes[i][0] + boxes[i][2] # to transform to pytorch bbox format
+            boxes[i][3] = boxes[i][1] + boxes[i][3]
+
+        label = torch.as_tensor(label)
+        label = torch.where(label==30,0,1)  # 0 if empty (categ id = 30), 1 if animal
+        image_id = torch.tensor([idx])
+        area = (boxes[:, 3] - boxes[:, 1]) * (boxes[:, 2] - boxes[:, 0])
+        iscrowd = torch.zeros((boxes.shape[0],), dtype=torch.int64)
+
+        target = {}
+        target["boxes"] = boxes
+        target["labels"] = label
+        target["image_id"] = image_id
+        target['area']=area
+        target['iscrowd']=iscrowd
+
+        # TO DO : resize all to same size
+
+        if self.transform:
+            # transform image AND target
+            image, target = self.transform(image, target)
+
+        return image, target
+
+
 # Model class
 class model:
     pass
